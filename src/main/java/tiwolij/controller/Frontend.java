37b3f913de6bd14383d9cad50de4ac2f517ce48e
Philip Schildkamp <philip.schildkamp@smail.uni-koeln.de>
2016-10-07 14:39:45 +0200

    Updated th:href structure, prototyped quote-preview

--- src/main/java/tiwolij/controller/Frontend.java
+++ src/main/java/tiwolij/controller/Frontend.java
@@ -1,17 +1,37 @@
 package tiwolij.controller;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import tiwolij.domain.Author;
+import tiwolij.domain.AuthorLocale;
+import tiwolij.domain.Quote;
+import tiwolij.domain.QuoteLocale;
+import tiwolij.domain.Work;
+import tiwolij.domain.WorkLocale;
+import tiwolij.service.author.AuthorService;
 import tiwolij.service.quote.QuoteService;
+import tiwolij.service.work.WorkService;
 
 @Controller
 @RequestMapping({ "", "/" })
 public class Frontend {
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private AuthorService authors;
+
+	@Autowired
+	private WorkService works;
 
 	@Autowired
 	private QuoteService quotes;
@@ -22,8 +42,30 @@ public class Frontend {
 	}
 
 	@GetMapping("/view")
-	public ModelAndView getView(@RequestParam(name = "id", defaultValue = "0") int id) {
-		return new ModelAndView("frontend").addObject("quote", quotes.getQuote(id));
+	public ModelAndView view(Locale locale, @RequestParam(name = "id", defaultValue = "0") Integer quoteId,
+			@RequestParam(name = "lang", defaultValue = "") String language) {
+		ModelAndView mv = new ModelAndView("frontend");
+
+		if (quoteId == 0)
+			return new ModelAndView("redirect:/");
+
+		if (language.isEmpty())
+			return new ModelAndView("redirect:/view?lang=" + locale.getLanguage() + "&id=" + quoteId);
+		
+		Quote quote = quotes.getQuote(quoteId);
+		QuoteLocale quoteLocale = quotes.getLocaleByLang(quoteId, language);
+
+		Work work = quote.getWork();
+		WorkLocale workLocale = works.getLocaleByLang(work.getId(), language);
+
+		Author author = work.getAuthor();
+		AuthorLocale authorLocale = authors.getLocaleByLang(author.getId(), language);
+		
+		mv.addObject("author", authorLocale);
+		mv.addObject("work", workLocale);
+		mv.addObject("quote", quoteLocale);
+		mv.addObject("image", author.getId());
+		return mv;
 	}
 
 }
