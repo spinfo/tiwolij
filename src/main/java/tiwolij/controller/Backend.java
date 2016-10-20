ca8e402ff232189cfd17f086d666edab211143bc
Philip Schildkamp <philip.schildkamp@smail.uni-koeln.de>
2016-10-20 15:07:58 +0200

    Corrected imports

--- src/main/java/tiwolij/controller/Backend.java
+++ src/main/java/tiwolij/controller/Backend.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import tiwolij.domain.Quote;
 import tiwolij.service.author.AuthorService;
 import tiwolij.service.quote.QuoteService;
 import tiwolij.service.work.WorkService;
