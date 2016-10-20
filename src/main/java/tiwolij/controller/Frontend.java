3e6c6ba7b280e6e2914a19ff5154e0c2197c7f0e
Philip Schildkamp <philip.schildkamp@smail.uni-koeln.de>
2016-10-20 17:45:33 +0200

    Overhauled frontend (unfinished)

--- src/main/java/tiwolij/controller/Frontend.java
+++ src/main/java/tiwolij/controller/Frontend.java
@@ -1,8 +1,11 @@
 package tiwolij.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.commons.lang3.tuple.Triple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,8 +36,37 @@ public class Frontend {
 	private QuoteService quotes;
 
 	@GetMapping({ "", "/" })
-	public String getRoot() {
-		return "redirect:/view";
+	public ModelAndView root() {
+		ModelAndView mv = new ModelAndView("frontend/index");
+		return mv;
+	}
+
+	@GetMapping("/list")
+	public ModelAndView list(Locale locale, @RequestParam(name = "schedule", defaultValue = "") String schedule,
+			@RequestParam(name = "lang", defaultValue = "") String language) {
+		ModelAndView mv = new ModelAndView("frontend/list");
+
+		if (schedule.isEmpty())
+			schedule = LocalDate.now().getDayOfMonth() + "-" + LocalDate.now().getMonthValue();
+
+		if (language.isEmpty())
+			language = locale.getLanguage();
+
+		List<QuoteLocale> locales = quotes.getLocalesByScheduleAndLang(schedule, language);
+		List<Triple<QuoteLocale, WorkLocale, AuthorLocale>> list = new ArrayList<Triple<QuoteLocale, WorkLocale, AuthorLocale>>();
+
+		WorkLocale w = null;
+		AuthorLocale a = null;
+
+		for (QuoteLocale q : locales) {
+			w = works.getLocaleByLang(q.getQuote().getWork().getId(), language);
+			a = authors.getLocaleByLang(w.getWork().getAuthor().getId(), language);
+			list.add(Triple.of(q, w, a));
+		}
+
+		mv.addObject("lang", language);
+		mv.addObject("list", list);
+		return mv;
 	}
 
 	@GetMapping("/view")
@@ -44,10 +76,10 @@ public class Frontend {
 
 		LocalDate now = LocalDate.now();
 		String schedule = now.getDayOfMonth() + "-" + now.getMonthValue();
-		
+
 		if (language.isEmpty())
 			language = locale.getLanguage();
-		
+
 		if (quoteId == 0)
 			quoteId = quotes.getLocaleByScheduleAndLang(schedule, language).getQuote().getId();
 
@@ -73,4 +105,12 @@ public class Frontend {
 		return mv;
 	}
 
+	@GetMapping("/random")
+	public String random(Locale locale) {
+		// TODO: Implement stub
+
+		Quote quote = new Quote();
+		return "redirect:/view?id=" + quote.getId() + "&lang=" + locale.getLanguage();
+	}
+	
 }
