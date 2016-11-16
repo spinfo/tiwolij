be3232a82e1928c66de4f80094a29b8dca28401f
Philip Schildkamp <philip.schildkamp@smail.uni-koeln.de>
2016-11-16 14:00:45 +0100

    Update deployment config

---
+++ src/main/java/tiwolij/WebInitializer.java
@@ -0,0 +1,13 @@
+package tiwolij;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TiwoliJ.class);
+	}
+
+}
