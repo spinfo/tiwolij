f5c89bbabe029782192c82399455fa9bed6c5f31
hermesj <hermesj@uni-koeln.de>
2016-11-23 13:35:36 +0100

    Refactored time randomizing

--- src/main/java/tiwolij/util/TimeRandomizer.java
+++ src/main/java/tiwolij/util/TimeRandomizer.java
@@ -11,13 +11,29 @@ public class TimeRandomizer {
 	
 	public static String getRandomizedTime(int startTime, int endTime){
 		
+		if(startTime<0||startTime>24){
+			startTime = 0;
+		}
+		
+		if(endTime<0||endTime>24){
+			endTime = 24;
+		}
+		
 		Random random = new Random();
+		
+		if(endTime<startTime){
+			endTime += 24;
+		}
 		int span = endTime-startTime;
 		if(span<0){
 			span = span*-1;
 		}
 		StringBuffer toReturn = new StringBuffer();
 		startTime = startTime + random.nextInt(span);
+		if(startTime>23){
+			startTime-=24;
+		}
+		
 		if(startTime<10){
 			toReturn.append("0");
 		}
